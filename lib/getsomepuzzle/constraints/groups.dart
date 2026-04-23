ac72e826a0de686fcb20f1493a38a8b33d62808f
Ghislain Lévêque <ghislain@leveque.cc>
2026-04-23 23:51:50 +0200

    Fix letter group

--- lib/getsomepuzzle/constraints/groups.dart
+++ lib/getsomepuzzle/constraints/groups.dart
@@ -253,7 +253,7 @@ class LetterGroup extends CellsCentricConstraint {
   @override
   bool verify(Puzzle puzzle) {
     // If any of my cells are not filled yet, there's no need to check further
-    final letterIndices = getAllIndicesForLetter(puzzle);
+    final letterIndices = (getAllIndicesForLetter(puzzle).toSet().union(indices.toSet())).toList();
     final myCellValues = puzzle.cellValues.indexed
         .where((elem) => letterIndices.contains(elem.$1))
         .map((elem) => elem.$2);
