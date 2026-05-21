7a0dee2fcc61ae2444e19854e3f003029b866155
Ghislain Lévêque <ghislain@leveque.cc>
2026-05-21 14:33:02 +0200

    Extract dashed-rect painter into shared helper

--- lib/widgets/create_page/dialogs/constraint_type_picker.dart
+++ lib/widgets/create_page/dialogs/constraint_type_picker.dart
@@ -13,6 +13,7 @@ import 'package:getsomepuzzle/getsomepuzzle/constraints/symmetry.dart';
 import 'package:getsomepuzzle/getsomepuzzle/constraints/to_flutter.dart';
 import 'package:getsomepuzzle/l10n/app_localizations.dart';
 import 'package:getsomepuzzle/widgets/column_count.dart';
+import 'package:getsomepuzzle/widgets/dashed_painter.dart';
 import 'package:getsomepuzzle/widgets/eyes.dart';
 import 'package:getsomepuzzle/widgets/row_count.dart';
 import 'package:getsomepuzzle/getsomepuzzle/constraints/row_count.dart';
@@ -302,79 +303,7 @@ class _MajorityPreviewPainter extends CustomPainter {
       ..style = PaintingStyle.stroke;
     final dashLength = borderWidth * 2;
     final gapLength = borderWidth * 1.5;
-    _drawDashedRect(canvas, rect, paint, dashLength, gapLength);
-  }
-
-  void _drawDashedRect(
-    Canvas canvas,
-    Rect rect,
-    Paint paint,
-    double dashLength,
-    double gapLength,
-  ) {
-    final path = Path();
-
-    final double left = rect.left;
-    final double top = rect.top;
-    final double right = rect.right;
-    final double bottom = rect.bottom;
-
-    _addDashedLine(
-      path,
-      Offset(left, top),
-      Offset(right, top),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(right, top),
-      Offset(right, bottom),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(right, bottom),
-      Offset(left, bottom),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(left, bottom),
-      Offset(left, top),
-      dashLength,
-      gapLength,
-    );
-
-    canvas.drawPath(path, paint);
-  }
-
-  void _addDashedLine(
-    Path path,
-    Offset start,
-    Offset end,
-    double dashLength,
-    double gapLength,
-  ) {
-    final totalLength = (end - start).distance;
-    final direction = (end - start) / totalLength;
-    final segmentLength = dashLength + gapLength;
-
-    double currentDistance = 0;
-    while (currentDistance < totalLength) {
-      final dashEnd = currentDistance + dashLength;
-      final actualDashEnd = dashEnd > totalLength ? totalLength : dashEnd;
-
-      final p1 = start + direction * currentDistance;
-      final p2 = start + direction * actualDashEnd;
-
-      path.moveTo(p1.dx, p1.dy);
-      path.lineTo(p2.dx, p2.dy);
-
-      currentDistance += segmentLength;
-    }
+    drawDashedRect(canvas, rect, paint, dashLength, gapLength);
   }
 
   @override
--- lib/widgets/majority.dart
+++ lib/widgets/majority.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:getsomepuzzle/getsomepuzzle/constraints/majority.dart';
 import 'package:getsomepuzzle/getsomepuzzle/model/constants.dart';
+import 'package:getsomepuzzle/widgets/dashed_painter.dart';
 
 const Color _mjBorderBlackTarget = Color(0xFF3A4A6B);
 const Color _mjBorderWhiteTarget = Color(0xFFC8D4E8);
@@ -65,79 +66,7 @@ class MajorityZonePainter extends CustomPainter {
       final dashLength = borderWidth * 2;
       final gapLength = borderWidth * 1.5;
 
-      _drawDashedRect(canvas, rect, borderPaint, dashLength, gapLength);
-    }
-  }
-
-  void _drawDashedRect(
-    Canvas canvas,
-    Rect rect,
-    Paint paint,
-    double dashLength,
-    double gapLength,
-  ) {
-    final path = Path();
-
-    final double left = rect.left;
-    final double top = rect.top;
-    final double right = rect.right;
-    final double bottom = rect.bottom;
-
-    _addDashedLine(
-      path,
-      Offset(left, top),
-      Offset(right, top),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(right, top),
-      Offset(right, bottom),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(right, bottom),
-      Offset(left, bottom),
-      dashLength,
-      gapLength,
-    );
-    _addDashedLine(
-      path,
-      Offset(left, bottom),
-      Offset(left, top),
-      dashLength,
-      gapLength,
-    );
-
-    canvas.drawPath(path, paint);
-  }
-
-  void _addDashedLine(
-    Path path,
-    Offset start,
-    Offset end,
-    double dashLength,
-    double gapLength,
-  ) {
-    final totalLength = (end - start).distance;
-    final direction = (end - start) / totalLength;
-    final segmentLength = dashLength + gapLength;
-
-    double currentDistance = 0;
-    while (currentDistance < totalLength) {
-      final dashEnd = currentDistance + dashLength;
-      final actualDashEnd = dashEnd > totalLength ? totalLength : dashEnd;
-
-      final p1 = start + direction * currentDistance;
-      final p2 = start + direction * actualDashEnd;
-
-      path.moveTo(p1.dx, p1.dy);
-      path.lineTo(p2.dx, p2.dy);
-
-      currentDistance += segmentLength;
+      drawDashedRect(canvas, rect, borderPaint, dashLength, gapLength);
     }
   }
 
